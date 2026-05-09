660001219acc8010578e8defc0049e0b0058aefb
DiegoPerino76 <271780@unifio.edu.br>
2026-05-09 17:33:21 -0300

    ex 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import CardAluno from './components/CardAluno'
 import ListaProdutos from './components/ListaProdutos'
+import Menu from './components/Menu'
 
 function App() {
   return (
     <div>
+      <Menu />
       <CardAluno />
       <ListaProdutos />
     </div>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,17 @@
+.menu {
+  display: flex;
+  gap: 20px;
+  background-color: #222;
+  padding: 15px;
+  border-radius: 10px;
+}
+
+.menu a {
+  color: white;
+  text-decoration: none;
+  transition: 0.3s;
+}
+
+.menu a:hover {
+  color: cyan;
+}
