0ed85ad49f3061ef6a1e396875401b9fd8c0b064
DiegoPerino76 <271780@unifio.edu.br>
2026-05-09 17:42:04 -0300

    ex 6

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import ListaProdutos from './components/ListaProdutos'
 import Menu from './components/Menu'
 import ListaAlunos from './components/ListaAlunos'
 import ListaFilmes from './components/ListaFilmes'
+import Titulo from './components/Titulo'
 
 function App() {
   return (
     <div>
       <Menu />
+      <Titulo />
       <CardAluno />
       <ListaProdutos />
       <ListaAlunos />
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,31 @@
+function Titulo() {
+
+  return (
+    <div>
+
+      <h1
+        style={{
+          color: 'blue',
+          fontSize: '40px',
+          backgroundColor: '#ddd',
+          padding: '20px'
+        }}
+      >
+        Meu Título
+      </h1>
+
+      <h2
+        style={{
+          color: 'gray',
+          fontSize: '25px',
+          padding: '10px'
+        }}
+      >
+        Meu Subtítulo
+      </h2>
+
+    </div>
+  )
+}
+
+export default Titulo
